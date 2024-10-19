96e4ec96dfda8df08b4976ec3cdf3334612e4de0
Ying Tong <yingtong0106@gmail.com>
2024-10-19 23:57:53 +0800

    Laid out components on App home page

--- src/App.jsx
+++ src/App.jsx
@@ -8,34 +8,79 @@ import "./components/Banner/banner.css";
 import "./components/Card/card.css";
 
 function App() {
+  const badgeColorSchemes = [
+    "grey",
+    "red",
+    "yellow",
+    "green",
+    "blue",
+    "indigo",
+    "purple",
+    "pink",
+  ];
+  const bannerVariants = ["success", "warning", "error", "neutral"];
+
+  function mapBadges(shape) {
+    return badgeColorSchemes.map((color) => {
+      return (
+        <Badge className={`badge ${shape} ${color}`}>
+          <Badge.Text>
+            <h1 className="badgeText">Badge</h1>
+          </Badge.Text>
+        </Badge>
+      );
+    });
+  }
+
+  function mapBanners(type) {
+    return bannerVariants.map((variant) => {
+      return (
+        <Banner className="banner" variant={variant} type={type}>
+          <Banner.Title className="bannerTitle">
+            Lorem ipsum dolor sit.
+          </Banner.Title>
+          {type === "multiline" && (
+            <Banner.Text className="bannerText">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Corrupti, ipsa.
+            </Banner.Text>
+          )}
+        </Banner>
+      );
+    });
+  }
+
   return (
     <>
       <div className="appContainer">
         <section className="badgeSection">
-          <Badge className="badge square red">
-            <Badge.Text>
-              <h1 className="badgeText">Badge Text</h1>
-            </Badge.Text>
-          </Badge>
-          <Badge className="badge pill red">
-            <Badge.Text>
-              <h1 className="badgeText">Badge Text</h1>
-            </Badge.Text>
-          </Badge>
+          <h2 className="sectionTitle">Badges</h2>
+          <div className="wrapper">
+            <div className="squareBadges">
+              <h4>Square</h4>
+              {mapBadges("square")}
+            </div>
+            <div className="pillBadges">
+              <h4>Pill</h4>
+              {mapBadges("pill")}
+            </div>
+          </div>
         </section>
         <section className="bannerSection">
-          <Banner className="banner single success">
-            <Banner.Title className="bannerTitle">Banner Title</Banner.Title>
-          </Banner>
-          <Banner className="banner success multiline">
-            <Banner.Title className="bannerTitle">Banner Title</Banner.Title>
-            <Banner.Text className="bannerText">Banner Text</Banner.Text>
-          </Banner>
+          <h2 className="sectionTitle">Banners</h2>
+          <div className="wrapper">
+            <h4>Single Line Banners</h4>
+            <div className="singleLineBanners">{mapBanners("single")}</div>
+            <div className="multiLineBanners">{mapBanners("multiline")}</div>
+          </div>
         </section>
         <section className="cardSection">
           <Card className="card">
             <Card.Title className="cardTitle">Card Title</Card.Title>
-            <Card.Text className="cardText">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis, ipsam.</Card.Text>
+            <Card.Text className="cardText">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis,
+              ipsam.
+            </Card.Text>
           </Card>
         </section>
       </div>
--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,10 +1,33 @@
 import React from "react";
 import { FaCheckCircle } from "react-icons/fa";
+import { IoWarning } from "react-icons/io5";
+import { VscError } from "react-icons/vsc";
+import { FaInfoCircle } from "react-icons/fa";
+import classNames from 'classnames'
 
-function Banner({ children, className }) {
+function Banner({ children, className, variant, type }) {
+  const classes = classNames(className, variant, type)
+
+  function getIcon() {
+    const className = 'icon'
+    if (variant === 'success') {
+      return <FaCheckCircle className={className} />
+    }
+    else if (variant === 'warning') {
+      return <IoWarning className={className} />
+    }
+    else if (variant === 'error') {
+      return <VscError className={className} />
+    }
+    else {
+      return <FaInfoCircle className={className} />
+    }
+
+  }
+
   return (
-    <div className={className}>
-      <FaCheckCircle className="icon" />
+    <div className={classes}>
+      {getIcon()}
       <div>{children}</div>
     </div>
   );
